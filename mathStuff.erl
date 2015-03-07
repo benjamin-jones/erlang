f702bfb8b0d2011f7512b38b615408daa4c49bcb
Benjamin Jones <ben@benjaminjones.me>
2015-03-07 02:16:35 -0500

    Added some list ops and pattern matching

--- mathStuff.erl
+++ mathStuff.erl
@@ -1,5 +1,5 @@
 -module(mathStuff).
--export([factorial/1, area/1]).
+-export([factorial/1, area/1, average/1, sum/1, len/1, double/1, member/2]).
 
 factorial(0) -> 1;
 factorial(N) -> N * factorial(N-1).
@@ -14,3 +14,18 @@ area({triangle, A, B, C}) ->
 	math:sqrt(S*(S-A)*(S-B)*(S-C));
 area(Other) ->
 	{invalid_object, Other}.
+
+average(X) -> sum(X) / len(X).
+
+sum([H|T]) -> H + sum(T);
+sum([]) -> 0.
+
+len([_|T]) -> 1 + len(T);
+len([]) -> 0.
+
+double([H|T]) -> [2*H|double(T)];
+double([]) -> [].
+
+member(H, [H|_]) -> true;
+member(H, [_|T]) -> member(H,T);
+member(_, []) -> false.
